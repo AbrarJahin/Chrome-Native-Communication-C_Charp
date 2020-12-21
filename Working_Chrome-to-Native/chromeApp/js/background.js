bdf245fdd7cf27d314739b424567ca63b5deef76
Abrar Jahin <abrarjahin@live.com>
2020-12-21 15:59:59 +0600

    Temporary Connected - CAllbacks are not connected

--- Working_Chrome-to-Native/chromeApp/js/background.js
+++ Working_Chrome-to-Native/chromeApp/js/background.js
@@ -1,22 +1,24 @@
+var hostName = "com.bcc.chrome.extension.native.communication";
+var port = null;
+
+//Startup Event
 chrome.runtime.onInstalled.addListener(function() {
 	console.log("OK");
 });
 
-// Bind event:
+// Bind event on receiving any message:
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	switch(message.eventType) {
 		case "signature":
+			sendNativeMessage({
+				"signXmlText": "OK",
+				"signReason": "sign Reason",
+				"signId": "sign Id"
+			});
+
 			var status = ["signed", "rejected"];
 			var signedText = "OK";
-			// setTimeout(function() {
-			// 	sendResponse({
-			// 		message: message,
-			// 		sender: sender,
-			// 		time : (new Date()).toUTCString() + "-" + (new Date()).getMilliseconds(),
-			// 		status: status,
-			// 		signedText: signedText
-			// 	});
-			// }, 4000);
+
 			sendResponse({
 				message: message,
 				sender: sender,
@@ -37,3 +39,39 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 			break;
 	}
 });
+
+//////////////////////////////////////////////////////////////////////////////
+
+function appendMessage(text) {
+	console.log(text);
+}
+
+function onNativeMessage(message){
+	appendMessage("Received message: <b>" + JSON.stringify(message) + "</b>");
+	port = null;	//Disconnect by self - try
+}
+
+function onDisconnected() {
+	appendMessage("Failed to connect: " + chrome.runtime.lastError.message);
+	port = null;
+}
+
+function connectNativeMessage() {
+	port = chrome.runtime.connectNative(hostName);
+	port.onMessage.addListener(onNativeMessage);
+	port.onDisconnect.addListener(onDisconnected);
+}
+
+function disconnectNativeMessage() {
+	try {
+		chrome.runtime.reload();
+	} catch (error) {
+		console.log("kios mode enabled");
+		chrome.runtime.restart();
+	}
+}
+
+function sendNativeMessage(message) {
+	connectNativeMessage();
+	port.postMessage(message);
+}
